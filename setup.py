98df4b600f12ad950bef0355fe8058d6af52c917
Shriyans Sudhi <71912662+shriyanss@users.noreply.github.com>
2024-02-09 08:59:58 +0530

    Basic feature to get subdomains from the given subdomain_list

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         ],
     },
     install_requires=[
-        "pyfiglet"
+        "pyfiglet",
+        "tldextract"
     ]
 )
--- subgen/subgen/cli.py
+++ subgen/subgen/cli.py
@@ -1,10 +1,19 @@
 import pyfiglet
 import argparse
 import os
+import tldextract
 
 def banner():
     banner_text = pyfiglet.figlet_format("subgen", font="slant")
     print(banner_text)
+
+def output(subdomains, output_file):
+    for subdomain in subdomains:
+        if subdomain != "":
+            print(subdomain)
+            if output_file != None:
+                open(output_file, 'a').write(subdomain + "\n")
+
 
 def main():
 
@@ -23,9 +32,26 @@ def main():
     if not os.path.exists(args.input):
         print("Input file does't exist: {}".format(args.input))
         exit(1)
-    if os.path.exists(args.output):
+    if args.output != None and os.path.exists(args.output):
         print("Output file already exist: {}".format(args.output))
         exit(1)
+    
+    new_wordlist = []
+    
+    # load the subdomain_list
+    subdomains_list = [line.strip() for line in open(args.input, 'r').readlines()]
+
+    # read the list
+    for subdomain in subdomains_list:
+        extracted = tldextract.extract(subdomain)
+        domain = "{}.{}".format(extracted.domain, extracted.suffix)
+        current_subdomains = subdomain.replace(f'.{domain}', '').replace(f'{domain}', '').split('.')
+        if len(current_subdomains) > 0:
+            for subd in current_subdomains:
+                if subd not in new_wordlist:
+                    new_wordlist.append(subd)
+    
+    output(new_wordlist, args.output)
 
 if __name__ == "__main__":
     main()
