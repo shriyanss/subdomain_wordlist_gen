68af5fc766a88c725b0e5f18f8a517990adae2b2
Shriyans Sudhi <71912662+shriyanss@users.noreply.github.com>
2024-02-09 08:43:02 +0530

    Verify arguments

--- subgen/subgen/cli.py
+++ subgen/subgen/cli.py
@@ -1,5 +1,6 @@
 import pyfiglet
 import argparse
+import os
 
 def banner():
     banner_text = pyfiglet.figlet_format("subgen", font="slant")
@@ -9,7 +10,7 @@ def main():
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-i', '--input', dest='input', default=None, help='Input file to generate wordlist with')
+    parser.add_argument('-i', '--input', dest='input', default=None, help='Input file to generate wordlist with', required=True)
     parser.add_argument('-s', '--silent', action="store_true", help='Don\'t print the banner')
     parser.add_argument('-o', '--output', dest='output', default=None, help='Output file to write the wordlist')
 
@@ -17,6 +18,14 @@ def main():
 
     if args.silent != True:
         banner()
+    
+    # verify arguments
+    if not os.path.exists(args.input):
+        print("Input file does't exist: {}".format(args.input))
+        exit(1)
+    if os.path.exists(args.output):
+        print("Output file already exist: {}".format(args.output))
+        exit(1)
 
 if __name__ == "__main__":
     main()
